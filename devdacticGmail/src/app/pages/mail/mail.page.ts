6431070dcbca0e4d17fba950ab4b9ac48fe8cd58
Developer-David <david.rodriguez.form@gmail.com>
2022-03-28 04:43:21 +0300

    David version - working project

--- devdacticGmail/src/app/pages/mail/mail.page.ts
+++ devdacticGmail/src/app/pages/mail/mail.page.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PopoverController } from '@ionic/angular';
 import { AccountPage } from '../account/account.page';
- 
+
 @Component({
   selector: 'app-mail',
   templateUrl: './mail.page.html',
@@ -11,23 +11,31 @@ import { AccountPage } from '../account/account.page';
 })
 export class MailPage implements OnInit {
   emails = [];
- 
-  constructor(private http: HttpClient, private popoverCtrl: PopoverController, private router: Router) { }
- 
+
+  constructor(
+    private http: HttpClient,
+    private popoverCtrl: PopoverController,
+    private router: Router
+  ) {}
+
   ngOnInit() {
-    this.http.get<any[]>('https://devdactic.fra1.digitaloceanspaces.com/gmail/data.json').subscribe(res => {
-      this.emails = res;
-      for (let e of this.emails) {
-        // Create a custom color for every email
-        e.color = this.intToRGB(this.hashCode(e.from));
-      }
-    });
+    this.http
+      .get<any[]>(
+        'https://devdactic.fra1.digitaloceanspaces.com/gmail/data.json'
+      )
+      .subscribe((res) => {
+        this.emails = res;
+        for (let e of this.emails) {
+          // Create a custom color for every email
+          e.color = this.intToRGB(this.hashCode(e.from));
+        }
+      });
   }
- 
+
   openDetails(id) {
     this.router.navigate(['tabs', 'mail', id]);
   }
- 
+
   // https://stackoverflow.com/questions/3426404/create-a-hexadecimal-colour-based-on-a-string-with-javascript
   private hashCode(str) {
     var hash = 0;
@@ -36,18 +44,27 @@ export class MailPage implements OnInit {
     }
     return hash;
   }
- 
+
   private intToRGB(i) {
-    var c = (i & 0x00FFFFFF)
-      .toString(16)
-      .toUpperCase();
- 
+    var c = (i & 0x00ffffff).toString(16).toUpperCase();
+
     return '#' + '00000'.substring(0, 6 - c.length) + c;
   }
- 
+
   doRefresh(ev) {
     setTimeout(() => {
       ev.target.complete();
     }, 2000);
   }
+
+  async openAccount(ev) {
+    const popover = await this.popoverCtrl.create({
+      component: AccountPage,
+      event: ev,
+      cssClass: 'custom-popover'
+    });
+   
+    await popover.present();
+  }
+  
 }
